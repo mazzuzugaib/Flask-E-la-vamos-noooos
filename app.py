170fa2e9bc7f3be7ae2b56d9c65bfc516a520614
mazzuzugaib <mazzuzugaib@gmail.com>
2025-04-11 21:02:39 -0300

    parei na aula 11
    coloquei as classes para as musicas e integrei na tabela da home.html

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-
+#imports
+from flask import Flask, render_template
+#instanciando o Flask
 app = Flask(__name__)
+
+class lista:
+    def __init__(self, titulo, artista, genero):
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
 
 @app.route('/')
 def inicio():
-    return('<h1>ola mundo é o caralho</h1>')
+    m1 = lista('Knight of Cydonia', 'Muse', 'Rock')
+    m2 = lista('Bohemian Rhapsody', 'Queen', 'Rock')
+    m3 = lista('Stairway to Heaven', 'Led Zeppelin', 'Rock')
 
-app.run()
+    return render_template('home.html', lista_musicas = [m1, m2, m3])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
