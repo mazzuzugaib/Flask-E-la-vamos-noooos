2fe391be3e0bf4022d8fa07fdcc24a2a71966727
mazzuzugaib <mazzuzugaib@gmail.com>
2025-04-12 17:02:28 -0300

    ate a 22
    adicionando a musica pelo formulario atraves do request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #instanciando o Flask
 app = Flask(__name__)
 
@@ -9,13 +9,34 @@ class lista:
         self.artista = artista
         self.genero = genero
 
+m1 = lista('Knight of Cydonia', 'Muse', 'Rock')
+m2 = lista('Bohemian Rhapsody', 'Queen', 'Rock')
+m3 = lista('Stairway to Heaven', 'Led Zeppelin', 'Rock')
+lista_musicas = [m1, m2, m3]
+
 @app.route('/')
 def inicio():
-    m1 = lista('Knight of Cydonia', 'Muse', 'Rock')
-    m2 = lista('Bohemian Rhapsody', 'Queen', 'Rock')
-    m3 = lista('Stairway to Heaven', 'Led Zeppelin', 'Rock')
 
-    return render_template('home.html', lista_musicas = [m1, m2, m3])
+    return render_template('home.html', lista_musicas = lista_musicas)
+
+#renderiza pagina de cadastro
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+#pega dados do form em cadastro.html.
+@app.route('/cadastrar', methods=['POST'])
+def cadastro_musica():
+    titulo = request.form['titulo']
+    artista = request.form['artista']
+    genero = request.form['genero']
+
+    nova_musica = lista(titulo, artista, genero)
+
+    lista_musicas.append(nova_musica)
+
+    return render_template('home.html', lista_musicas = lista_musicas)
+
 
 
 if __name__ == '__main__':
