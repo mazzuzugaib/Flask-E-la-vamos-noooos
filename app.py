b4f01b40717d1f7dcea01e7c6bc91b57fdab9a7e
mazzuzugaib <mazzuzugaib@gmail.com>
2025-04-14 17:03:46 -0300

    ate a 26
    colocado sessao de usuario

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #imports
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 #instanciando o Flask
 app = Flask(__name__)
+
+app.secret_key = 'satorarepotenetoperarotas'
 
 class lista:
     def __init__(self, titulo, artista, genero):
@@ -14,15 +16,28 @@ m2 = lista('Bohemian Rhapsody', 'Queen', 'Rock')
 m3 = lista('Stairway to Heaven', 'Led Zeppelin', 'Rock')
 lista_musicas = [m1, m2, m3]
 
+
+
+#VIEWS---------------------------------------------------------------------------
+
+#pagina inicial
 @app.route('/')
 def inicio():
 
     return render_template('home.html', lista_musicas = lista_musicas)
 
-#renderiza pagina de cadastro
+#pagina de cadastro
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+#login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#ROTAS----------------------------------------------------------------------------------------------
 
 #pega dados do form em cadastro.html.
 @app.route('/cadastrar', methods=['POST'])
@@ -37,13 +52,11 @@ def cadastro_musica():
 
     return render_template('home.html', lista_musicas = lista_musicas)
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
+#validação de usuário
 @app.route('/validar', methods=['POST'])
 def valide():
     if request.form['user'] == 'lorem' and request.form['senha'] == 'ipsum':
+        session['usuario_in'] = request.form['user']
         return render_template('home.html', lista_musicas = lista_musicas)
     return render_template('login.html')
 
