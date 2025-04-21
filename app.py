977ba8abeaec6345383cffefde0ed7359d6c6b66
mazzuzugaib <mazzuzugaib@gmail.com>
2025-04-21 11:58:40 -0300

    ate 90
    -configuração do bd
    -classe/coluna das musicas
    -classe/coluna dos usuarios

--- app.py
+++ app.py
@@ -7,7 +7,36 @@ app = Flask(__name__)
 app.secret_key = 'satorarepotenetoperarotas'
 
 #configurando o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+      SGBD = 'mysql+mysqlconnector',
+      usuario = 'root',
+      senha = 'tenet',
+      servidor = 'localhost',
+      database = 'playmusica'
+    )
+
+db = SQLAlchemy(app)
+
+#criando a tabela de músicas
+class Musica(db.Model):
+    tb_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tb_titulo = db.Column(db.String(30), nullable=False)
+    tb_artista = db.Column(db.String(30), nullable=False)
+    tb_genero = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return '<Name> %r' % self.name
+
+#criando a tabela de usuários
+class Usuario(db.Model):
+    id_us = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_us = db.Column(db.String(50), nullable=False)
+    login_us = db.Column(db.String(10), nullable=False)
+    senha_us = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return '<Name> %r' % self.name
 
 class lista:
     def __init__(self, titulo, artista, genero):
