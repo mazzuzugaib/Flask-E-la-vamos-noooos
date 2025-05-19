db75926262e199fa7cfb28a1f4e6bc1b24cb6be0
mazzuzugaib <mazzuzugaib@gmail.com>
2025-05-19 11:22:09 -0300

    128
    -criado token de validação do projeto

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 #para criar o banco de dados
 from flask_sqlalchemy import SQLAlchemy
+#validacao
+from flask_wtf.csrf import CSRFProtect
 
 #instanciando o Flask
 app = Flask(__name__)
@@ -10,6 +12,7 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 from views import *
 
